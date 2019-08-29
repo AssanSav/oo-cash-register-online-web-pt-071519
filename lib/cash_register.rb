aa48c9669346c92e95a656fb0871b53085b6be3c
assane savadogo <assane.savadogo81@gmail.com>
2019-08-29 00:49:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 class CashRegister
+ attr_accessor :total, :discount, :last_transaction
  
+ def initialize(discount = nil)
+   @discount = discount
+   @total = total
+   @items = items
+ end
+ 
+ def add_item(title, price, quantity = 1)
+   @total = (self.total.to_f * discount/ 100).to_i
+   
+ end
 end
